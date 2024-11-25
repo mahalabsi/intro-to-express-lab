6fd74af2f492fcc3aca6529b44264d685b3e2dab
mahalabsi <mahatalabsi@gmail.com>
2024-11-25 20:14:58 +0300

    t22

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
 //task1
+
 app.get('/greetings/:name', (req, res) => {
   res.send(`<h1>Hello there,${req.params.name} </h1>`)
 })
+//task2
 
 app.get('/roll/:number', (req, res) => {
   const number = req.params.number
-  if (number !== req.params.number) {
-    res.send(`You must specify a number.`)
+  if (isNaN(number)) {
+    res.send('You must specify a number.')
   } else {
     const randomNumber = Math.floor(Math.random() * (parseInt(number) + 1))
     res.send(`You rolled a ${randomNumber}.`)
