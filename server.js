6b3e40ae31e6e2fa9f0e766a69a082a7dcbe308d
mahalabsi <mahatalabsi@gmail.com>
2024-11-25 21:14:07 +0300

    task4

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ const collectibles = [
   { name: 'autographed picture of a dog', price: 10 },
   { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
 ]
+
+const shoes = [
+  { name: 'Birkenstocks', price: 50, type: 'sandal' },
+  { name: 'Air Jordans', price: 500, type: 'sneaker' },
+  { name: 'Air Mahomeses', price: 501, type: 'sneaker' },
+  { name: 'Utility Boots', price: 20, type: 'boot' },
+  { name: 'Velcro Sandals', price: 15, type: 'sandal' },
+  { name: 'Jet Boots', price: 1000, type: 'boot' },
+  { name: 'Fifty-Inch Heels', price: 175, type: 'heel' }
+]
+
 //task1
 
 app.get('/greetings/:name', (req, res) => {
@@ -22,6 +33,7 @@ app.get('/roll/:number', (req, res) => {
   }
 })
 
+//task3
 app.get('/collectibles/:index', (req, res) => {
   const index = parseInt(req.params.index)
   if (isNaN(index) || index < 0 || index >= collectibles.length) {
@@ -32,6 +44,33 @@ app.get('/collectibles/:index', (req, res) => {
       `So, you want the ${item.name}? For ${item.price}, it can be yours!`
     )
   }
+})
+
+//task4
+app.get('/shoes', (req, res) => {
+  const minPrice = req.query.minPrice
+  const maxPrice = req.query.maxPrice
+  const type = req.query.type
+
+  let filteredShoes = shoes
+
+  if (minPrice) {
+    filteredShoes = filteredShoes.filter(
+      (shoe) => shoe.price >= parseInt(minPrice)
+    )
+  }
+
+  if (maxPrice) {
+    filteredShoes = filteredShoes.filter(
+      (shoe) => shoe.price <= parseInt(maxPrice)
+    )
+  }
+
+  if (type) {
+    filteredShoes = filteredShoes.filter((shoe) => shoe.type === type)
+  }
+
+  res.send(filteredShoes)
 })
 
 app.listen(3000, () => {
