923357878cbb9b9248037fd6eff060102c18725f
mahalabsi <mahatalabsi@gmail.com>
2024-11-25 20:39:04 +0300

    task3

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+const collectibles = [
+  { name: 'shiny ball', price: 5.95 },
+  { name: 'autographed picture of a dog', price: 10 },
+  { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+]
 //task1
 
 app.get('/greetings/:name', (req, res) => {
@@ -14,6 +19,18 @@ app.get('/roll/:number', (req, res) => {
   } else {
     const randomNumber = Math.floor(Math.random() * (parseInt(number) + 1))
     res.send(`You rolled a ${randomNumber}.`)
+  }
+})
+
+app.get('/collectibles/:index', (req, res) => {
+  const index = parseInt(req.params.index)
+  if (isNaN(index) || index < 0 || index >= collectibles.length) {
+    res.send('This item is not yet in stock. Check back soon!')
+  } else {
+    const item = collectibles[index]
+    res.send(
+      `So, you want the ${item.name}? For ${item.price}, it can be yours!`
+    )
   }
 })
 
